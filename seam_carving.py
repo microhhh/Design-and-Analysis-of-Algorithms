80a76a0c69b528e02d87b3d2b94dae2b8f0dab21
付博 <13654840887@163.com>
2018-10-25 19:53:58 +0800

    seam_carving

--- seam_carving.py
+++ seam_carving.py
@@ -1,5 +1,4 @@
 import sys
-
 from tqdm import trange
 import numpy as np
 from imageio import imread, imwrite
@@ -7,22 +6,22 @@ from scipy.ndimage.filters import convolve
 
 
 def calc_energy(img):
+    #Sobel filter for horizontal
     filter_du = np.array([
         [1.0, 2.0, 1.0],
         [0.0, 0.0, 0.0],
         [-1.0, -2.0, -1.0],
     ])
-    # This converts it from a 2D filter to a 3D filter, replicating the same
-    # filter for each channel: R, G, B
+    # 3D horizontal filter for each channel: R, G, B
     filter_du = np.stack([filter_du] * 3, axis=2)
 
+    #Sobel filter for Vertical
     filter_dv = np.array([
         [1.0, 0.0, -1.0],
         [2.0, 0.0, -2.0],
         [1.0, 0.0, -1.0],
     ])
-    # This converts it from a 2D filter to a 3D filter, replicating the same
-    # filter for each channel: R, G, B
+    # 3D Vertical filter for each channel: R, G, B
     filter_dv = np.stack([filter_dv] * 3, axis=2)
 
     img = img.astype('float32')
@@ -30,9 +29,7 @@ def calc_energy(img):
 
     # We sum the energies in the red, green, and blue channels
     energy_map = convolved.sum(axis=2)
-
     return energy_map
-
 
 def crop_c(img, scale_c):
     r, c, _ = img.shape
@@ -43,76 +40,57 @@ def crop_c(img, scale_c):
 
     return img
 
-
 def crop_r(img, scale_r):
-    img = np.rot90(img, 1, (0, 1))
+    img = np.rot90(img, 1)
     img = crop_c(img, scale_r)
-    img = np.rot90(img, 3, (0, 1))
+    img = np.rot90(img, -1)
     return img
 
-
 def carve_column(img):
-    r, c, _ = img.shape
-
+    width, height, _ = img.shape
     M, backtrack = minimum_seam(img)
-    mask = np.ones((r, c), dtype=np.bool)
+    mask = np.ones((width, height), dtype=np.bool)
 
     j = np.argmin(M[-1])
-    for i in reversed(range(r)):
+    for i in reversed(range(width)):
         mask[i, j] = False
         j = backtrack[i, j]
 
     mask = np.stack([mask] * 3, axis=2)
-    img = img[mask].reshape((r, c - 1, 3))
+    img = img[mask].reshape((width, height - 1, 3))
     return img
 
-
 def minimum_seam(img):
-    r, c, _ = img.shape
+    width, height, _ = img.shape
     energy_map = calc_energy(img)
-
     M = energy_map.copy()
     backtrack = np.zeros_like(M, dtype=np.int)
 
-    for i in range(1, r):
-        for j in range(0, c):
-            # Handle the left edge of the image, to ensure we don't index a -1
+    #dynamic programming with M(i,j)
+    for i in range(1, width):
+        for j in range(0, height):
             if j == 0:
-                idx = np.argmin(M[i - 1, j:j + 2])
-                backtrack[i, j] = idx + j
-                min_energy = M[i - 1, idx + j]
+                offset = np.argmin(M[i - 1, j:j + 2])
+                backtrack[i, j] = offset + j
+                min_energy = M[i - 1, offset + j]
             else:
-                idx = np.argmin(M[i - 1, j - 1:j + 2])
-                backtrack[i, j] = idx + j - 1
-                min_energy = M[i - 1, idx + j - 1]
+                offset = np.argmin(M[i - 1, j - 1:j + 2])
+                backtrack[i, j] = offset + j - 1
+                min_energy = M[i - 1, offset + j - 1]
 
             M[i, j] += min_energy
 
     return M, backtrack
 
+if __name__ == '__main__':
 
-def main():
-    if len(sys.argv) != 5:
-        print('usage: carver.py <r/c> <scale> <image_in> <image_out>', file=sys.stderr)
-        sys.exit(1)
-
-    which_axis = sys.argv[1]
-    scale = float(sys.argv[2])
-    in_filename = sys.argv[3]
-    out_filename = sys.argv[4]
+    in_filename = sys.argv[1]
+    out_filename = sys.argv[2]
+    scale = 0.5
 
     img = imread(in_filename)
-
-    if which_axis == 'r':
-        out = crop_r(img, scale)
-    elif which_axis == 'c':
-        out = crop_c(img, scale)
-    else:
-        print('usage: carver.py <r/c> <scale> <image_in> <image_out>', file=sys.stderr)
-        sys.exit(1)
-
+    print('------caving in columns--------')
+    out = crop_c(img, scale)
+    print('------caving in rows-----------')
+    out = crop_r(out, scale)
     imwrite(out_filename, out)
-
-
-if __name__ == '__main__':
-    main()
